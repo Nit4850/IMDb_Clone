ee750a217c7ea773f67349e92dd53668cb4b36be
Nit4850 <nitesh10048@gmail.com>
2023-03-23 11:50:14 +0530

    initial commit

---
+++ src/components/Slide.jsx
@@ -0,0 +1,65 @@
+
+import { styled,Typography } from '@mui/material';
+import React from 'react'
+
+
+
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+const responsive = {
+    
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 3
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+};
+
+const StyledBanner = styled('img')`
+  width: 100%;
+  margin-top:20px
+
+`
+
+const Title = styled(Typography)`
+    color: #FFFFFF;
+    display: flex;
+`;
+
+
+
+const Slide = ({movies}) => {
+  return (
+    <Carousel 
+    swipeable={false}
+    draggable={false}
+    responsive={responsive}
+    infinite={true}
+    autoPlay={true}
+    autoPlaySpeed={3000}
+    keyBoardControl={true}
+    showDots={false}
+    slidesToSlide={1}
+   >
+       {
+            movies.map(movie => (
+                <>
+                    <StyledBanner key={movie.id} src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} />
+                    <Title>{movie.original_title}</Title>
+                </>
+           ))
+       }
+
+   </Carousel>
+  )
+}
+
+export default Slide
---
+++ src/components/common/HeaderMenu.jsx
@@ -0,0 +1,34 @@
+
+import { Menu,MenuItem } from '@mui/material'
+import React from 'react'
+import {Link} from 'react-router-dom'
+import { routhPath } from '../../constants/route'
+
+
+const HeaderMenu = ({open, handleClose}) => {
+
+    const openMenu = Boolean(open);
+    return (
+        <Menu
+            id="basic-menu"
+            anchorEl={open}
+            open={openMenu}
+            onClose={handleClose}
+            MenuListProps={{
+                'aria-labelledby': 'basic-button',
+            }}
+        >
+            <Link to={`${routhPath.categories}?catogory=popular`} style ={{textDecoration: 'none', color: 'inherit'}}>
+                <MenuItem onClick={handleClose}>Popular</MenuItem>
+            </Link>    
+            <Link to={`${routhPath.categories}?catogory=toprated`} style ={{textDecoration: 'none', color: 'inherit'}}>
+                <MenuItem onClick={handleClose}>Top Rated</MenuItem>
+            </Link>
+            <Link to={`${routhPath.categories}?catogory=upcoming`} style ={{textDecoration: 'none', color: 'inherit'}}>
+                <MenuItem onClick={handleClose}>Upcoming</MenuItem>
+            </Link>
+        </Menu>
+    )
+}
+
+export default HeaderMenu
